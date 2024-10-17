250f6b87e520d8613bb710e8f329a3d0c2d3f9af
gandhi <gandhiflores@gmail.com>
2024-10-16 23:24:23 -0500

    inicio de persistencia de datos

---
+++ persistencia01/src/main/java/com/tecsup/persistencia01/Persistencia01Application.java
@@ -0,0 +1,13 @@
+package com.tecsup.persistencia01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Persistencia01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Persistencia01Application.class, args);
+	}
+
+}
---
+++ persistencia01/src/test/java/com/tecsup/persistencia01/Persistencia01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecsup.persistencia01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Persistencia01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
